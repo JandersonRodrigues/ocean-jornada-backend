ca3c5991e0bc00690828e998532bef7ec4fa007a
Janderson James Rodrigues <37557313+JandersonRodrigues@users.noreply.github.com>
2024-02-21 13:24:14 +0000

    Adicionando Express e novas rotas

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+
+const express = require('express') 
+const app = express() 
+
+app.get('/', function (req, res) { 
+    res.send('Hello World')
+})
+
+app.get('/oi', function (req,res) {
+    res.send("Olá Mundo!")
+})
+
+app.listen(3000)
